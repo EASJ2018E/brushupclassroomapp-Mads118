a78678a4d5507c365854f522fcead30b4050aac2
Mads <vpmads@gmail.com>
2018-09-09 13:28:47 +0200

    Første opgave lavet

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        private string name;
+        private int birthmonth;
+        private int birthday;
+
+        public Student(string studentName, int studentBirthmonth, int studentBirthday)
+        {
+            studentName = name;
+            studentBirthmonth = birthmonth;
+            studentBirthday = birthday;
+        }
+
+        public string studentName { get; }
+        public int studentBirthmonth { get; }
+        public int studentBirthday { get; }
+    }
+}
