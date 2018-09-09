d194beacae5afa3379897ef8a061b101fb7034ed
Mads <vpmads@gmail.com>
2018-09-09 13:44:32 +0200

    opgave 2 lavet

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class ClassRoom
+    {
+        private string classname { get; set; }
+        private List<Student> classlist { get; set; }
+        private DateTime semesterstart { get; set; }
+
+        public ClassRoom()
+        {
+        }
+    }
+   
+}
